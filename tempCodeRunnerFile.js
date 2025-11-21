a17a079c29f3d9eb4cab65ff3d2b3de3f12af73a
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-11-21 16:32:43 +0530

    Remove TypeScript package and related temporary files

--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let myNumber = [1, 312, 313];
-console.log(myNumber);
-//# sourceMappingURL=tempCodeRunnerFile.js.map
--- tempCodeRunnerFile.ts
+++
@@ -1,2 +0,0 @@
-let myNumber: number[] = [1,312,313]
-console.log(myNumber)
